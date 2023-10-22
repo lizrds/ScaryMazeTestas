b25d20ec46327775e20ed53db9749d69ce769120
lizrds <creeperbum789@gmail.com>
2023-10-22 17:30:23 +0200

    FinishedGame
    atleiskit kad nedariau nuoseklai visko, bet viena dalyka padarius kazkaip uzsimirsau ir labai greitai viska padariau. Pridejau jumpscare, kitus lygius, teleportacija ir viska kas reikia. Viena problema ta, kad palietus siena zaidejas nenusiteleportuoja i spawna.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,22 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-    
+    public GameObject jumpScare;
+    public string sceneName;
+    public int speed;
+    public int spawnX;
+    public int spawnY;
+    Rigidbody2D rb;
+    public Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector2(mousePosition.x, mousePosition.y);
-        print(mousePosition.ToString());
-        print(transform.position.ToString());
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        rb.MovePosition(mousePosition);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Finish"))
+        {
+            print("Collision");
+            SceneManager.LoadScene(sceneName);
+            transform.position = new Vector2(spawnX, spawnY);
+        }
+
+        if (collision.gameObject.name.Contains("JumpScare"))
+        {
+            GetComponent<AudioSource>().Play();
+            jumpScare.SetActive(true);
+            
+
+            
+        }
+        if (collision.gameObject.name.Contains("Wall"))
+        {
+            
+            mousePosition.x = spawnX; mousePosition.y = spawnY;
+            
+        }
+        print(collision.gameObject.name);
+    }
+
 }
